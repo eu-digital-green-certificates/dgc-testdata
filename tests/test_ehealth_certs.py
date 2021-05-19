7274dc34571ecba6680a6bd0e487772bf3c85cca
Bhavin Sanghvi <bhavin.sanghvi@qryptal.com>
2021-05-19 09:49:32 +0800

    dgc-testdata - verification script - draft - WIP version

--- tests/test_ehealth_certs.py
+++ tests/test_ehealth_certs.py
@@ -26,7 +26,8 @@
 # Python 3.9
 # pip install wheel base45 jsonschema jsonref filecache cose pytest pyzbar Pillow python-dateutil
 
-from csv import DictWriter
+from pathlib import Path
+from pytest import fixture
 from glob import glob
 from binascii import hexlify, unhexlify
 from cbor2 import loads, CBORTag
@@ -36,7 +37,7 @@ from cose.keys.keyparam import KpAlg, EC2KpX, EC2KpY, EC2KpCurve, RSAKpE, RSAKpN
 from cose.keys.keyops import VerifyOp
 from cose.keys.keytype import KtyEC2, KtyRSA
 from cose.curves import P256
-from cose.messages import Sign1Message, CoseMessage
+from cose.messages import Sign1Message
 from cose.headers import KID
 from base64 import b64decode
 from base45 import b45decode
@@ -53,6 +54,8 @@ from cryptography.hazmat.primitives.hashes import SHA256
 from io import BytesIO
 from PIL.Image import open as image_open
 from pyzbar.pyzbar import decode as bar_decode
+from os import path
+
 
 VERSION = '1.1.0'
 VALID_FROM, VALID_UNTIL = 'VALID_FROM_ts', 'VALID_UNTIL_ts'
@@ -80,85 +83,113 @@ VALIDATION_CLOCK = 'VALIDATIONCLOCK'
 COSE = 'COSE'
 TEST_CONTEXT = 'TESTCTX'
 CERTIFICATE = 'CERTIFICATE'
-
-CSV_COLUMN_ORDER = ['test_set',
-                    'error',
-                    EXPECTED_PICTURE_DECODE,
-                    EXPECTED_UN_PREFIX,
-                    EXPECTED_B45DECODE,
-                    EXPECTED_EXPIRATION_CHECK,
-                    EXPECTED_VERIFY]
+QR_CODE = '2DCODE'
+PREFIX = 'PREFIX'
+BASE45 = 'BASE45'
+COMPRESSED = 'COMPRESSED'
 
 
-def test_one():
-    test_set = r'.\..\ES\2DCode\raw\101.json'
-    test_summary = []
-    _set_test(test_set, test_summary)
-    csv_file = 'test_one.csv'
-    with open(csv_file, 'w', newline='') as output_file:
-        dict_writer = DictWriter(output_file, CSV_COLUMN_ORDER, extrasaction='ignore')
-        dict_writer.writeheader()
-        dict_writer.writerows(test_summary)
-    print('Test passed!')
+def pytest_generate_tests(metafunc):
+    if "config_env" in metafunc.fixturenames:
+        country_code = metafunc.config.getoption("country_code")
+        file_name = metafunc.config.getoption("file_name")
+        print(country_code, file_name)
+        test_dir = path.dirname(path.abspath(__file__))
+        test_files = glob(rf'{test_dir}\..\{country_code}\*\*\{file_name}.json', recursive=True)
+        ids = [Path(*Path(test_file).parts[-4:]).as_posix() for test_file in test_files]
+        metafunc.parametrize("config_env", test_files, indirect=True, ids=ids)
 
 
-def test_all():
-    print('test_all started...')
-    test_summary = []
-    test_sets = glob('.\\..\\**\\*.json', recursive=True)
-    test_count = 0
-    for test_set in test_sets:
-        _set_test(test_set, test_summary)
-        test_count += 1
-        # if test_count > 250:
-        #     break
-    csv_file = 'test_summary.csv'
-    with open(csv_file, 'w', newline='') as output_file:
-        dict_writer = DictWriter(output_file, CSV_COLUMN_ORDER, extrasaction='ignore')
-        dict_writer.writeheader()
-        dict_writer.writerows(test_summary)
-    print('Test passed!')
+@fixture
+def config_env(request):
+    with open(request.param, encoding='utf8') as test_file:
+        return load(test_file)
 
 
-def _set_test(test_set, test_summary):
-    with open(test_set, encoding='utf8') as test_file:
-        # print(f'Testing {test_set}')
-        test_result = {"test_set": test_set, "error": ""}
-        test_result = _dgc_test(load(test_file), test_result)
-        test_summary.append(test_result)
+@fixture
+def dgc(config_env: Dict) -> Sign1Message:
+    if COSE in config_env.keys():
+        cbor_bytes = unhexlify(config_env[COSE])
+        cbor_object = loads(cbor_bytes)
+        if isinstance(cbor_object, CBORTag):  # Tagged Cose Object
+            decoded = Sign1Message.decode(cbor_bytes)
+        else:  # Un-tagged Cose Object
+            decoded = Sign1Message.from_cose_obj(cbor_object)
+        return decoded
+    # else:
+    #     raise ValueError(f'Missing Test Data {COSE}')
 
 
-def _dgc_test(dgc_example: Dict, test_result: Dict):
-    if EXPECTED_RESULTS in dgc_example.keys():
-        _picture_decode_test(dgc_example, test_result)
-        _un_prefix_test(dgc_example, test_result)
-        _b45decode_test(dgc_example, test_result)
-        if {COSE, TEST_CONTEXT} <= dgc_example.keys() and CERTIFICATE in dgc_example[TEST_CONTEXT].keys():
-            try:
-                cose_object = _get_cose_object(dgc_example[COSE])
-                decoded_payload = loads(cose_object.payload)
-                key, keyid, dsc_supported_operations, dsc_not_valid_before, dsc_not_valid_after = \
-                    _get_cert(dgc_example[TEST_CONTEXT][CERTIFICATE])
-                cose_object.key = key
-                _expiration_check(dgc_example, decoded_payload, dsc_not_valid_before, dsc_not_valid_after, test_result)
-                _verification_check(dgc_example, cose_object, keyid, test_result)
-            except Exception as ex:
-                test_result['error'] += repr(ex)
+@fixture
+def dsc(config_env: Dict):
+    if TEST_CONTEXT in config_env.keys() and CERTIFICATE in config_env[TEST_CONTEXT].keys():
+        cert_code = config_env[TEST_CONTEXT][CERTIFICATE]
+        x = y = e = n = None
+        cert = x509.load_pem_x509_certificate(
+            f'-----BEGIN CERTIFICATE-----\n{cert_code}\n-----END CERTIFICATE-----'.encode())
+        pub = cert.public_key().public_numbers()
+        fingerprint = cert.fingerprint(SHA256())
+        keyid = fingerprint[0:8]
+
+        if cert.signature_algorithm_oid == SignatureAlgorithmOID.RSA_WITH_SHA256:
+            e = int_to_bytes(pub.e)
+            n = int_to_bytes(pub.n)
+        elif cert.signature_algorithm_oid == SignatureAlgorithmOID.ECDSA_WITH_SHA256:
+            x = int_to_bytes(pub.x)
+            y = int_to_bytes(pub.y)
         else:
-            test_result['error'] += f'Missing Test Data {COSE} or {TEST_CONTEXT},'
-    return test_result
+            raise Exception(
+                f'Unsupported Certificate Algorithm: {cert.signature_algorithm_oid} for verification.'
+            )
+        try:
+            dsc_supported_operations = {eku.dotted_string for eku in
+                                        cert.extensions.get_extension_for_class(x509.ExtendedKeyUsage).value}
+        except ExtensionNotFound:
+            dsc_supported_operations = set()
+        dsc_not_valid_before = cert.not_valid_before.replace(tzinfo=timezone.utc)
+        dsc_not_valid_after = cert.not_valid_after.replace(tzinfo=timezone.utc)
+        key = None
+        if x and y:
+            key = CoseKey.from_dict(
+                {
+                    KpKeyOps: [VerifyOp],
+                    KpKty: KtyEC2,
+                    EC2KpCurve: P256,  # Ought o be pk.curve - but the two libs clash
+                    KpAlg: Es256,  # ECDSA using P-256 and SHA-256
+                    EC2KpX: x,
+                    EC2KpY: y,
+                }
+            )
+        elif e and n:
+            key = CoseKey.from_dict(
+                {
+                    KpKeyOps: [VerifyOp],
+                    KpKty: KtyRSA,
+                    KpAlg: Ps256,  # RSASSA-PSS using SHA-256 and MGF1 with SHA-256
+                    RSAKpE: e,
+                    RSAKpN: n,
+                }
+            )
+        return key, keyid, dsc_supported_operations, dsc_not_valid_before, dsc_not_valid_after
+    # else:
+    #     raise ValueError(f'Missing Test Data {CERTIFICATE}')
 
 
-def _verification_check(dgc_example: Dict, cose_object: Sign1Message, keyid, test_result: Dict):
-    if EXPECTED_VERIFY in dgc_example[EXPECTED_RESULTS].keys():
-        test_result[EXPECTED_VERIFY] = f"{dgc_example[EXPECTED_RESULTS][EXPECTED_VERIFY]}"
-        if KID in cose_object.phdr.keys():
-            given_kid = cose_object.phdr[KID]
-        else:
-            given_kid = cose_object.uhdr[KID]
-        result = 'Pass' if (cose_object.verify_signature() and keyid == given_kid) == \
-                           dgc_example[EXPECTED_RESULTS][EXPECTED_VERIFY] else 'Failed'
-        test_result[EXPECTED_VERIFY] += f':{result}'
+def test_verification_check(config_env: Dict, dgc: Sign1Message, dsc):
+    if EXPECTED_VERIFY in config_env[EXPECTED_RESULTS].keys():
+        given_kid = None
+        if COSE in config_env.keys():
+            if KID in dgc.phdr.keys():
+                given_kid = dgc.phdr[KID]
+            else:
+                given_kid = dgc.uhdr[KID]
+        if config_env[EXPECTED_RESULTS][EXPECTED_VERIFY]:
+            assert dsc[1] == given_kid
+            dgc.key = dsc[0]
+            assert dgc.verify_signature()
+        elif dgc and dsc and dsc[0]:
+            dgc.key = dsc[0]
+            assert not all((dsc[1] == given_kid, dgc.verify_signature()))
 
 
 def _expiration_check(dgc_example: Dict, decoded_payload: Dict, dsc_not_valid_before: datetime,
@@ -185,77 +216,12 @@ def _expiration_check(dgc_example: Dict, decoded_payload: Dict, dsc_not_valid_be
             test_result['error'] += f'Missing Test Data {VALIDATION_CLOCK},'
 
 
-def _get_cert(cert_code):
-    x = y = e = n = None
-    cert = x509.load_pem_x509_certificate(
-        f'-----BEGIN CERTIFICATE-----\n{cert_code}\n-----END CERTIFICATE-----'.encode())
-    pub = cert.public_key().public_numbers()
-    fingerprint = cert.fingerprint(SHA256())
-    keyid = fingerprint[0:8]
-
-    if cert.signature_algorithm_oid == SignatureAlgorithmOID.RSA_WITH_SHA256:
-        e = int_to_bytes(pub.e)
-        n = int_to_bytes(pub.n)
-    elif cert.signature_algorithm_oid == SignatureAlgorithmOID.ECDSA_WITH_SHA256:
-        x = int_to_bytes(pub.x)
-        y = int_to_bytes(pub.y)
-    else:
-        raise Exception(
-            f'Unsupported Certificate Algorithm: {cert.signature_algorithm_oid} for verification.'
-        )
-    try:
-        dsc_supported_operations = {eku.dotted_string for eku in
-                                    cert.extensions.get_extension_for_class(x509.ExtendedKeyUsage).value}
-    except ExtensionNotFound:
-        dsc_supported_operations = set()
-    dsc_not_valid_before = cert.not_valid_before.replace(tzinfo=timezone.utc)
-    dsc_not_valid_after = cert.not_valid_after.replace(tzinfo=timezone.utc)
-    key = None
-    if x and y:
-        key = CoseKey.from_dict(
-            {
-                KpKeyOps: [VerifyOp],
-                KpKty: KtyEC2,
-                EC2KpCurve: P256,  # Ought o be pk.curve - but the two libs clash
-                KpAlg: Es256,  # ECDSA using P-256 and SHA-256
-                EC2KpX: x,
-                EC2KpY: y,
-            }
-        )
-    elif e and n:
-        key = CoseKey.from_dict(
-            {
-                KpKeyOps: [VerifyOp],
-                KpKty: KtyRSA,
-                KpAlg: Ps256,  # RSASSA-PSS using SHA-256 and MGF1 with SHA-256
-                RSAKpE: e,
-                RSAKpN: n,
-            }
-        )
-    return key, keyid, dsc_supported_operations, dsc_not_valid_before, dsc_not_valid_after
-
-
-def _get_cose_object(cose_code: str):
-    cbor_bytes = unhexlify(cose_code)
-    cbor_object = loads(cbor_bytes)
-    # print(loads(cbor_bytes).keys())
-    if isinstance(cbor_object, CBORTag):  # Tagged Cose Object
-        decoded = CoseMessage.decode(cbor_bytes)
-    else:  # Un-tagged Cose Object
-        decoded = Sign1Message.from_cose_obj(cbor_object)
-    return decoded
-
-
-def _b45decode_test(dgc_example: Dict, test_result: Dict):
-    if EXPECTED_B45DECODE in dgc_example[EXPECTED_RESULTS].keys():
-        test_result[EXPECTED_B45DECODE] = f"{dgc_example[EXPECTED_RESULTS][EXPECTED_B45DECODE]}"
-        if {'BASE45', 'COMPRESSED'} <= dgc_example.keys():
-            result = 'Pass' if (_get_compressed(dgc_example['BASE45']) == dgc_example['COMPRESSED'].lower()) \
-                               == dgc_example[EXPECTED_RESULTS][EXPECTED_B45DECODE] else 'Failed'
-            test_result[EXPECTED_B45DECODE] += f':{result}'
+def test_b45decode(config_env: Dict):
+    if EXPECTED_B45DECODE in config_env[EXPECTED_RESULTS].keys() and {BASE45, COMPRESSED} <= config_env.keys():
+        if config_env[EXPECTED_RESULTS][EXPECTED_B45DECODE]:
+            assert (_get_compressed(config_env[BASE45]) == config_env[COMPRESSED].lower())
         else:
-            test_result[EXPECTED_B45DECODE] += f':NotRun'
-            test_result['error'] += 'Missing Test Data BASE45 or COMPRESSED,'
+            assert not (_get_compressed(config_env[BASE45]) == config_env[COMPRESSED].lower())
 
 
 def _get_compressed(base45_code):
@@ -266,31 +232,20 @@ def _get_compressed(base45_code):
         return None
 
 
-def _un_prefix_test(dgc_example: Dict, test_result: Dict):
-    if EXPECTED_UN_PREFIX in dgc_example[EXPECTED_RESULTS].keys():
-        test_result[EXPECTED_UN_PREFIX] = f"{dgc_example[EXPECTED_RESULTS][EXPECTED_UN_PREFIX]}"
-        if {'BASE45', 'PREFIX'} <= dgc_example.keys():
-
-            result = 'Pass' if (f"HC1:{dgc_example['BASE45']}" == dgc_example['PREFIX']) == \
-                               dgc_example[EXPECTED_RESULTS][EXPECTED_UN_PREFIX] else 'Failed'
-            test_result[EXPECTED_UN_PREFIX] += f':{result}'
+def test_un_prefix(config_env: Dict):
+    if EXPECTED_UN_PREFIX in config_env[EXPECTED_RESULTS].keys() and {BASE45, PREFIX} <= config_env.keys():
+        if config_env[EXPECTED_RESULTS][EXPECTED_UN_PREFIX]:
+            assert (f"HC1:{config_env[BASE45]}" == config_env[PREFIX])
         else:
-            test_result[EXPECTED_UN_PREFIX] += f':NotRun'
-            test_result['error'] += 'Missing Test Data BASE45 or PREFIX,'
+            assert not (f"HC1:{config_env[BASE45]}" == config_env[PREFIX])
 
 
-def _picture_decode_test(dgc_example: Dict, test_result: Dict):
-    if EXPECTED_PICTURE_DECODE in dgc_example[EXPECTED_RESULTS].keys():
-        test_result[EXPECTED_PICTURE_DECODE] = f"{dgc_example[EXPECTED_RESULTS][EXPECTED_PICTURE_DECODE]}"
-        if {'2DCODE', 'PREFIX'} <= dgc_example.keys():
-            result = 'Pass' if (_get_code_content(dgc_example['2DCODE']) == dgc_example['PREFIX']) \
-                                == dgc_example[EXPECTED_RESULTS][EXPECTED_PICTURE_DECODE] else 'Fail'
-            test_result[EXPECTED_PICTURE_DECODE] += f':{result}'
+def test_picture_decode(config_env: Dict):
+    if EXPECTED_PICTURE_DECODE in config_env[EXPECTED_RESULTS].keys() and {QR_CODE, PREFIX} <= config_env.keys():
+        if config_env[EXPECTED_RESULTS][EXPECTED_PICTURE_DECODE]:
+            assert (_get_code_content(config_env[QR_CODE]) == config_env[PREFIX])
         else:
-            test_result[EXPECTED_PICTURE_DECODE] += f':NotRun'
-            test_result['error'] += 'Missing Test Data 2DCODE or PREFIX,'
-    else:
-        test_result[EXPECTED_PICTURE_DECODE] = ''
+            assert not (_get_code_content(config_env[QR_CODE]) == config_env[PREFIX])
 
 
 def _get_code_content(base64_image):
