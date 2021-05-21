cdfea703baabc5f9c507decf34abcd64295d8b5a
Bhavin Sanghvi <bhavin.sanghvi@qryptal.com>
2021-05-21 08:58:53 +0800

    Optimise Test Data Output Format

--- tests/test_ehealth_certs.py
+++ tests/test_ehealth_certs.py
@@ -390,7 +390,8 @@ def test_expected_key_usage(config_env: Dict):
     assert len(cose_payload[PAYLOAD_HCERT]) == 1
     assert 1 in cose_payload[PAYLOAD_HCERT].keys()
     hcert = cose_payload[PAYLOAD_HCERT][1]
-    assert len(set(hcert.keys()) & {'v', 'r', 't'}) == 1, 'DGC adheres to schema but contains multiple certificates'
+    assert len(set(hcert.keys()) & {'v', 'r', 't'}) == 1, \
+        'DGC adheres to schema but contains multiple certificates of different types'
     hcert_type = (set(hcert.keys()) & {'v', 'r', 't'}).pop()
 
     if config_env[EXPECTED_RESULTS][EXPECTED_KEY_USAGE] and dsc_supported_operations:
